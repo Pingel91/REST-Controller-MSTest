3d51cd52c2167cd8bc8451e6875933db91e2f066
Patrick Nyskjold <patr447g@edu.easj.dk>
2021-10-26 10:54:05 +0200

    Completed ManageMusicRecords + Upstart on MusicRecordsController

---
+++ RestControllerPairProgramming/RestControllerPairProgramming/Controllers/MusicRecordsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelLib.Model;
+using RestControllerPairProgramming.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestControllerPairProgramming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicRecordsController : ControllerBase
+    {
+        private readonly IManageMusicRecords mgr = new ManageMusicRecords();
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<MusicRecords> Get()
+        {
+            return mgr.Get();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public MusicRecords Get(int id)
+        {
+            return mgr.Get(id);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- RestControllerPairProgramming/RestControllerPairProgramming/Managers/ManageMusicRecords.cs
+++ RestControllerPairProgramming/RestControllerPairProgramming/Managers/ManageMusicRecords.cs
@@ -10,31 +10,54 @@ namespace RestControllerPairProgramming.Managers
     {
         private static List<MusicRecords> musicRecordsList = new List<MusicRecords>()
         {
-
+            new MusicRecords(1, "Crying In The Sun", "Faustix", "Dance", 196, 2018),
+            new MusicRecords(2, "Starfields", "Hallman", "Rock", 162, 2018),
+            new MusicRecords(3, "Happy Now", "Kygo", "Dance", 211, 2018),
+            new MusicRecords(4, "Breaking Me", "Topic", "Rock", 167, 2019)
         };
         public IEnumerable<MusicRecords> Get()
         {
-            throw new NotImplementedException();
+            return new List<MusicRecords>(musicRecordsList);
         }
 
         public MusicRecords Get(int id)
         {
-            throw new NotImplementedException();
+            if (musicRecordsList.Exists(i => i.Id == id))
+            {
+                return musicRecordsList.Find(i => i.Id == id);
+            }
+            throw new KeyNotFoundException();
         }
 
         public bool Create(MusicRecords value)
         {
-            throw new NotImplementedException();
+            musicRecordsList.Add(value);
+            return true;
         }
 
         public bool Update(int id, MusicRecords value)
         {
-            throw new NotImplementedException();
+            MusicRecords musicRecords = Get(id);
+            if (musicRecords != null)
+            {
+                musicRecords.Id = value.Id;
+                musicRecords.Title = value.Title;
+                musicRecords.Artist = value.Artist;
+                musicRecords.Genre = value.Genre;
+                musicRecords.Duration = value.Duration;
+                musicRecords.PublicationYear = value.PublicationYear;
+
+                return true;
+            }
+
+            return false;
         }
 
         public MusicRecords Delete(int id)
         {
-            throw new NotImplementedException();
+            MusicRecords musicRecords = Get(id);
+            musicRecordsList.Remove(musicRecords);
+            return musicRecords;
         }
     }
 }
