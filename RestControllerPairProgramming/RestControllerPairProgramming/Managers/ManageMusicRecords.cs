02eed076bf4ed395eb395cdac612ac35a58f9bca
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2021-10-26 10:34:56 +0200

    setup of ASP.NET RESTController + ModelLib - until ManageMusicRecords

---
+++ RestControllerPairProgramming/RestControllerPairProgramming/Managers/ManageMusicRecords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelLib.Model;
+
+namespace RestControllerPairProgramming.Managers
+{
+    public class ManageMusicRecords : IManageMusicRecords
+    {
+        private static List<MusicRecords> musicRecordsList = new List<MusicRecords>()
+        {
+
+        };
+        public IEnumerable<MusicRecords> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MusicRecords Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(MusicRecords value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, MusicRecords value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MusicRecords Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
